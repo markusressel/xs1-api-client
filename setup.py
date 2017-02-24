5db645d7cf44b520d04ee56cf1ad852f9892683c
Markus Ressel <markus9100@gmail.com>
2017-02-24 23:27:57 +0100

    changed version setup.py to include subdirectories

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='xs1_api_client',
-    version='1.0',
+    version='1.0.0',
     description='A library to get and set values of the EZcontrol XS1 Gateway',
     license='GPLv3+',
     author='Markus Ressel',
     author_email='mail@markusressel.de',
     url='https://www.markusressel.de',
-    packages=['xs1_api_client'],
+    packages=find_packages(),
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
