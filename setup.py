a70f797d38934dbb980fc2f340bf4bfe0de875bb
Markus Ressel <markus9100@gmail.com>
2017-02-25 02:03:00 +0100

    v1.0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='xs1_api_client',
-    version='1.0.0',
+    version='1.0.1',
     description='A library to get and set values of the EZcontrol XS1 Gateway',
     license='GPLv3+',
     author='Markus Ressel',
